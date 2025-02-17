6f57f37784152840397285b75aa24e2a0605c1bc
abhishek <abhishekhebballi85@gmail.com>
2025-02-17 15:25:51 +0530

    gfg weather django project

---
+++ gfg_weather/main/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+# import json to load json data to python dictionary
+import json
+# urllib.request to make a request to api
+import urllib.request
+
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        ''' api key might be expired use your own api_key
+            place api_key in place of appid="your api_key here "  '''
+
+        # source contain json data from api
+
+        source = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c551cdbebc5f82e27cea98c4a2012026').read()
+
+        # converting json data to dictionary
+
+        list_of_data = json.loads(source)
+        print(list_of_data)
+
+        # data for variable list_of_data
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data={}
+    return render(request, "main/index.html",data)
